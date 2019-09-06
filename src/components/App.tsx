e25db93b323a302d9cb655295d428eaaec8cac93
kboul <kos.voulgaridis@gmail.com>
2019-09-06 15:54:42 +0300

    Import Main component on App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Main from './Main';
 
 const App: React.FC = () => {
-    return (
-        <div>Hello World</div>
-    );
-}
+    return <Main />;
+};
 
 export default App;

c11293e64f01fffb71d130eea2f4fcec1275dbde
kboul <kos.voulgaridis@gmail.com>
2019-09-13 11:59:36 +0300

    Updates

---
+++ precache-manifest.d3f61dcd8c0e1f2f274b1e5f9d259a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f107ff0357d126994c2195ea22fc8ed",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "42a723add8382a4441ac",
+    "url": "/react-todo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2feda91907f308274a48",
+    "url": "/react-todo/static/css/main.8d2b5373.chunk.css"
+  },
+  {
+    "revision": "42a723add8382a4441ac",
+    "url": "/react-todo/static/js/2.33cefb11.chunk.js"
+  },
+  {
+    "revision": "2feda91907f308274a48",
+    "url": "/react-todo/static/js/main.2d59f79c.chunk.js"
+  },
+  {
+    "revision": "924314404a1f6066076d",
+    "url": "/react-todo/static/js/runtime~main.99f41c43.js"
+  }
+]);

45cb540212953b27c28bc04a621d1d925c47d2e4
kboul <kos.voulgaridis@gmail.com>
2019-11-13 14:04:38 +0200

    Unit tests for Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Todo from './Todo';
 import { todos as todosData } from '../mockData/todos';
 import TodoForm from './TodoForm';
 import { ITodo } from '../models/ITodo';
 
 const Main: React.SFC = () => {
-    const [todos, setTodos] = useState<Array<ITodo>>(todosData);
+    const [todos, setTodos] = React.useState<Array<ITodo>>(todosData);
 
     const addTodo = (text: string) => {
         const newTodos: Array<ITodo> = [...todos, { text, isCompleted: false }];
@@ -25,19 +25,20 @@ const Main: React.SFC = () => {
     };
 
     return (
-        <div className="app">
-            <div className="todoList">
-                <ul className="list-group">
-                    {todos.map((todo: ITodo, id: number) => (
+        <div data-test="component-main">
+            <ul className="list-group">
+                {todos.map((todo: ITodo, id: number) => (
+                    <div data-test="component-todo" key={id}>
                         <Todo
-                            key={id}
                             index={id}
                             todo={todo}
                             completeTodo={completeTodo}
                             removeTodo={removeTodo}
                         />
-                    ))}
-                </ul>
+                    </div>
+                ))}
+            </ul>
+            <div data-test="component-todo-form">
                 <TodoForm addTodo={addTodo} />
             </div>
         </div>
---
+++ src/components/__tests__/Main.test.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Main from '../Main';
+import { findByTestAttr } from '../../test/testUtils';
+import { todos } from '../../mockData/todos';
+
+const setup = () => {
+    return shallow(<Main />);
+};
+
+test('Main renders without error', () => {
+    const wrapper = setup();
+    const componentMain = findByTestAttr(wrapper, 'component-main');
+    expect(componentMain).toHaveLength(1);
+});
+
+describe('renders Todo & TodoForm components', () => {
+    const mockSetTodos = jest.fn();
+    React.useState = jest.fn(() => [todos, mockSetTodos]);
+
+    const wrapper = setup();
+
+    describe('Todo component', () => {
+        const todoWrapper = findByTestAttr(wrapper, 'component-todo');
+
+        test('renders Todo component without error', () => {
+            expect(todoWrapper).toHaveLength(todos.length);
+        });
+
+        test('Todo has object prop todo & completeTodo, removeTodo function props', () => {
+            const componentTodo = todoWrapper.find('Todo');
+
+            todos.forEach((todo, index) => {
+                expect(componentTodo.at(index).prop('todo')).toBeInstanceOf(
+                    Object
+                );
+            });
+
+            todos.forEach((todo, index) => {
+                expect(
+                    componentTodo.at(index).prop('completeTodo')
+                ).toBeInstanceOf(Function);
+            });
+
+            todos.forEach((todo, index) => {
+                expect(
+                    componentTodo.at(index).prop('removeTodo')
+                ).toBeInstanceOf(Function);
+            });
+        });
+
+        test('completeTodo logic runs as expected', () => {
+            const firstTodo = wrapper.find('Todo').at(0);
+            (firstTodo.props() as any).completeTodo(0);
+            wrapper.update();
+            expect(mockSetTodos).toHaveBeenCalled();
+        });
+
+        test('removeTodo logic runs as expected', () => {
+            const firstTodo = wrapper.find('Todo').at(0);
+            (firstTodo.props() as any).removeTodo(1);
+            wrapper.update();
+            expect(mockSetTodos).toHaveBeenCalled();
+        });
+    });
+
+    describe('TodoForm component', () => {
+        const todoFormWrapper = findByTestAttr(wrapper, 'component-todo-form');
+
+        test('renders TodoForm component without error', () => {
+            expect(todoFormWrapper).toHaveLength(1);
+        });
+
+        test('TodoForm has addTodo function prop', () => {
+            expect(
+                todoFormWrapper.find('TodoForm').prop('addTodo')
+            ).toBeInstanceOf(Function);
+        });
+
+        test('addTodo logic runs as expected', () => {
+            const todoForm = todoFormWrapper.find('TodoForm');
+            (todoForm.props() as any).addTodo();
+            wrapper.update();
+            expect(mockSetTodos).toHaveBeenCalled();
+        });
+    });
+});
